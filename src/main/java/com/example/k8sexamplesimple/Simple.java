1897682bdd5b2b75feda676191b3b5cc0f57cefe
te <ytrakhimovich@competo.io>
2023-02-10 10:33:34 +0000

    container configuration: env (for short list of params, not secret and stable)

--- src/main/java/com/example/k8sexamplesimple/Simple.java
+++ src/main/java/com/example/k8sexamplesimple/Simple.java
@@ -2,6 +2,7 @@ package com.example.k8sexamplesimple;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class Simple {
 
+    @Value("${app.custom.message}")
+    private String customMessage;
+
     @GetMapping("/")
     public String simple() {
-        return "Hello simple";
+        return customMessage;
     }
 }
